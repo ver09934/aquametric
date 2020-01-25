8e0ef36728714d46d3509afc7185f9f4722e335e
ver09934 <ianvernooy@gmail.com>
2020-01-24 19:07:33 -0500

    Make path to test file absolute

--- aquametric/home.py
+++ aquametric/home.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from io import BytesIO
+import os
 
 bp = Blueprint(__name__, __name__)
 
@@ -40,7 +41,7 @@ def submit():
     content = request.get_json(silent=False)
     # return jsonify({"uuid":uuid})
     # with open("test.txt", "a") as f:
-    with open(os.path.join(os.path.dirname(__file__), "test.txt") "a") as f:
+    with open(os.path.join(os.path.dirname(__file__), "test.txt"), "a") as f:
         f.write(str(content))
         f.write("\n")
     return jsonify({"Success": True})
