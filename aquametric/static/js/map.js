865a44dc781aa8ccf0fbb01c1e82f5fd74f509eb
ver09934 <ianvernooy@gmail.com>
2020-02-26 22:42:25 -0500

    Javascript is weird

--- aquametric/static/js/map.js
+++ aquametric/static/js/map.js
@@ -1,35 +1,51 @@
 $(document).ready(function(){
 
-var stageMarker = L.icon({
-    iconUrl: '/static/images/stageMarker.png',
+    var stageMarker = L.icon({
+        iconUrl: '/static/images/stageMarker.png',
+        iconSize:     [28.8, 45], // size of the icon
+        iconAnchor:   [16, 50],   // point of the icon which will correspond to marker's location
+        popupAnchor:  [16, -50]   // point from which the popup should open relative to the iconAnchor
+    });
 
-    iconSize:     [28.8, 45], // size of the icon
-    iconAnchor:   [16, 50], // point of the icon which will correspond to marker's location
-    popupAnchor:  [16, -50] // point from which the popup should open relative to the iconAnchor
-});
+    var mymap = L.map('map').setView([42.784723, -73.842862], 8);
+    var group = L.layerGroup()
 
-var mymap = L.map('map').setView([42.784723, -73.842862], 8);
-var group = L.layerGroup()
-var info;
+    var Hydda_Full = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
 
-var Hydda_Full = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+    var locations = [
+        L.latLng(42.784723, -73.842862),
+        L.latLng(43.1, -74.1)
+    ];
 
-var locations = [
-    L.latLng(42.784723, -73.842862),
+    for (var i = 0; i < locations.length; i++) {
+        var marker = L.marker(locations[i], {icon: stageMarker});
+        marker.on('click', wrapper(i));
+        marker.addTo(mymap);
+    }
 
-];
-var i;
-var id = 0;
-for(i = 0; i < locations.length; i++) {
-    var marker = L.marker(locations[i], {icon: stageMarker}).on('click',markerOnClick).addTo(mymap);
-}
+    // e is the marker index
+    // val is the click data from leaflet
+    function wrapper(e){ 
+        return function(val){
+            console.log(val);
+            console.log(e);
+            markerOnClick();
+        };
+    }
+
+    /*
+    for (var i = 0; i < locations.length; i++) {
+        var marker = L.marker(locations[i], {icon: stageMarker});.on('click', markerOnClick).addTo(mymap);
+    }
+    */
     
-function markerOnClick(e) {
-    document.getElementById("flash").style.display = "block";
-    $("#flash").fadeIn(350);
-    $("#flash").fadeOut(350);
-}
+    function markerOnClick() {
+        document.getElementById("flash").style.display = "block";
+        $("#flash").fadeIn(350);
+        $("#flash").fadeOut(350);
+    }
+
 });
