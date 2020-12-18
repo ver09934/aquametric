a6824cd661d689451c27540cbcf4b19a1a47a3f0
rmenon1008 <rmenon1008@gmail.com>
2020-12-18 10:38:27 -0500

    Added last-connected feature

--- aquametric/static/js/map.js
+++ aquametric/static/js/map.js
@@ -54,6 +54,8 @@ $(document).ready(function(){
                     
                     console.log("Current data:");
                     console.log(logData);
+                    var date = new Date(logData["published_at"]);
+                    $("#last-seen").html(" · last connected " + timeago(Date.now() - date.getTime()));
 
                     // fields = ["stage", "temp", "turbidity", "conductivity"];
                     fields = ["stage", "temp", "conductivity"];
@@ -73,3 +75,48 @@ $(document).ready(function(){
 
     });
 });
+
+function timeago(ms) {
+
+  var ago = Math.floor(ms / 1000);
+  var part = 0;
+
+  if (ago < 60) { return "seconds ago"; }
+
+  if (ago < 120) { return "a minute ago"; }
+  if (ago < 3600) {
+    while (ago >= 60) { ago -= 60; part += 1; }
+    return part + " minutes ago";
+  }
+
+  if (ago < 7200) { return "an hour ago"; }
+  if (ago < 86400) {
+    while (ago >= 3600) { ago -= 3600; part += 1; }
+    return part + " hours ago";
+  }
+
+  if (ago < 172800) { return "a day ago"; }
+  if (ago < 604800) {
+    while (ago >= 172800) { ago -= 172800; part += 1; }
+    return part + " days ago";
+  }
+
+  if (ago < 1209600) { return "a week ago"; }
+  if (ago < 2592000) {
+    while (ago >= 604800) { ago -= 604800; part += 1; }
+    return part + " weeks ago";
+  }
+
+  if (ago < 5184000) { return "a month ago"; }
+  if (ago < 31536000) {
+    while (ago >= 2592000) { ago -= 2592000; part += 1; }
+    return part + " months ago";
+  }
+
+  if (ago < 1419120000) { // 45 years, approximately the epoch
+    return "more than year ago";
+  }
+
+  // TODO pass in Date.now() and ms to check for 0 as never
+  return "never";
+}
